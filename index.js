0284b2e543ed516e91f3ea95ad1f12e401b05149
DreamWalker <servantofgovinda@gmail.com>
2020-10-10 00:10:18 +0530

    Update index.js
    Changing sensitivity to 15 for carousel

--- index.js
+++ index.js
@@ -71,10 +71,10 @@ $(".carousel").on("touchstart", function(event){
         var xClick = event.originalEvent.touches[0].pageX;
     $(this).one("touchmove", function(event){
         var xMove = event.originalEvent.touches[0].pageX;
-        if( Math.floor(xClick - xMove) > 5 ){
+        if( Math.floor(xClick - xMove) > 15 ){
             $(this).carousel('next');
         }
-        else if( Math.floor(xClick - xMove) < -5 ){
+        else if( Math.floor(xClick - xMove) < -15 ){
             $(this).carousel('prev');
         }
     });
